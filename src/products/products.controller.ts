d7725b0e07dc273a8b51ece6ff98628c57fff32e
moawedmohamed <moawedmohamed144@gmail.com>
2026-03-16 02:04:24 +0200

    CRUD operation created

--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -1,13 +1,57 @@
-import { Controller, Get } from '@nestjs/common';
-
-@Controller()
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  NotFoundException,
+  Param,
+  Post,
+  Put,
+} from '@nestjs/common';
+import { type CreateProductsDto } from './dtos/create-products.dto';
+import { UpdateProduct } from './dtos/update-product.dto';
+type ProductType = { id: number; title: string; price: number };
+@Controller('api/products')
 export class ProductsController {
-    @Get("/api/products")
+  private products: ProductType[] = [
+    { id: 1, title: 'book', price: 10 },
+    { id: 2, title: 'pen', price: 5 },
+    { id: 3, title: 'laptop', price: 500 },
+  ];
+  @Post()
+  public createProducts(@Body() body: CreateProductsDto) {
+    const newProducts: ProductType = {
+      id: this.products.length + 1,
+      title: body.title,
+      price: body.price,
+    };
+    this.products.push(newProducts);
+    return newProducts;
+  }
+  @Get()
   public getAllProducts() {
-    return [
-      { id: 1, title: 'book', price: 10 },
-      { id: 2, title: 'pen', price: 5 },
-      { id: 3, title: 'laptop', price: 500 },
-    ];
+    return this.products;
+  }
+  @Get('/api/products/:id')
+  public getProductById(@Param('id') id: string) {
+    const product = this.products.find((p) => p.id === parseInt(id));
+    if (!product) throw new NotFoundException();
+    return product;
+  }
+  @Put('/:id')
+  public updateProductById(
+    @Body() body: UpdateProduct,
+    @Param('id') id: string,
+  ) {
+    const product = this.products.find((p) => p.id === parseInt(id));
+    if (!product) throw new NotFoundException();
+    console.log(body);
+    return { message: 'products updates ' };
+  }
+  @Delete('/:id')
+  public deleteProductsById(@Param('id') id: string) {
+    const product = this.products.find((p) => p.id === parseInt(id));
+    if (!product) throw new NotFoundException();
+    return { message: 'product deleted ' };
   }
 }
---
+++ src/users/users.controller.ts
@@ -0,0 +1,13 @@
+import { Controller, Get } from '@nestjs/common';
+
+@Controller()
+export class UsersController {
+  @Get('/api/products')
+  public getAllProducts() {
+    return [
+      { id: 1, title: 'book', price: 10 },
+      { id: 2, title: 'pen', price: 5 },
+      { id: 3, title: 'laptop', price: 500 },
+    ];
+  }
+}
